2118bd4687432999d746cba8fe289ec191b58e82
Atsuya Tanaka <tenmusu007@gmail.com>
2024-05-09 15:31:43 +0900

    2024/0504

---
+++ challeng/20240510/Record_type.ts
@@ -0,0 +1,19 @@
+type MyReadonly<T> = {
+  readonly [K in keyof T]: T[K];
+}
+
+/* _____________ テストケース _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<MyReadonly<Todo1>, Readonly<Todo1>>>,
+]
+
+interface Todo1 {
+  title: string
+  description: string
+  completed: boolean
+  meta: {
+    author: string
+  }
+}
