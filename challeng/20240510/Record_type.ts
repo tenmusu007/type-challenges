290aa25e7bdea6acf5f7ed037eef819c82505a99
Atsuya Tanaka <tenmusu007@gmail.com>
2024-05-10 14:50:10 +0900

    2024/0510

--- challeng/20240510/Record_type.ts
+++ challeng/20240510/Record_type.ts
@@ -1,19 +1,42 @@
 type MyReadonly<T> = {
   readonly [K in keyof T]: T[K];
-}
+};
 
 /* _____________ テストケース _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from '@type-challenges/utils';
 
-type cases = [
-  Expect<Equal<MyReadonly<Todo1>, Readonly<Todo1>>>,
-]
+type cases = [Expect<Equal<MyReadonly<Todo1>, Readonly<Todo1>>>];
 
 interface Todo1 {
-  title: string
-  description: string
-  completed: boolean
+  title: string;
+  description: string;
+  completed: boolean;
   meta: {
-    author: string
-  }
+    author: string;
+  };
 }
+
+const test: Test = {
+  title: 'hoge',
+  description: 'aaa',
+};
+
+const testFunc = (arg: Test) => {
+  return true;
+};
+
+const Arg = {
+  title: 'hoge',
+  description: 'test',
+  meta: {
+    author: 'haha',
+  },
+};
+type Test = {
+  readonly title: string;
+  description: string;
+  meta: {
+    readonly author: string;
+  };
+};
+testFunc(Arg);
