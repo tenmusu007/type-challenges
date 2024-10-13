08eaebd6d8cf53655cf999157b1b09c929b018fb
Atsuya Tanaka <tenmusu007@gmail.com>
2024-10-13 13:57:25 +0900

    new type

--- challeng/easy/Array/2024_09_29.ts
+++ challeng/easy/Array/2024_09_29.ts
@@ -39,7 +39,7 @@ import type { Equal, Expect } from "@type-challenges/utils";
 type cases = [
 	Expect<Equal<First<[3, 2, 1]>, 3>>,
 	Expect<Equal<Second<[3, 2, 1]>, 2>>,
-	Expect<Equal<Last<[3, 2, 1]>, 1>>,
+	Expect<Equal<Last<[3, 2, 1]>, 1>>, 
 	Expect<Equal<First<[() => 123, { a: string }]>, () => 123>>,
 	Expect<Equal<First<[]>, never>>,
 	Expect<Equal<First<[undefined]>, undefined>>,
