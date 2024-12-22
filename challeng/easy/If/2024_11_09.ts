6c5e1a7dd2b5767bd431bed201c678c94fd9eaf7
Atsuya Tanaka <tenmusu007@gmail.com>
2024-12-22 09:07:23 +0900

    Includes

--- challeng/easy/If/2024_11_09.ts
+++ challeng/easy/If/2024_11_09.ts
@@ -29,7 +29,7 @@ type cases = [
   Expect<Equal<If<true, 'a', 'b'>, 'a'>>,
   Expect<Equal<If<false, 'a', 2>, 2>>,
   Expect<Equal<If<boolean, 'a', 2>, 'a' | 2>>,
-] 
+]
 
 // @ts-expect-error
 type error = If<null, 'a', 'b'>
